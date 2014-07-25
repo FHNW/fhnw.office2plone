74f76cf472e980195e5d377023ede0811d0eef4e
Tom Gross <itconsense@gmail.com>
2014-07-25 13:23:30 +0200

    extract images and try drag&drop

--- src/fhnw/office2plone/browser/docx_importer.py
+++ src/fhnw/office2plone/browser/docx_importer.py
@@ -1,10 +1,17 @@
 from Products.Five import BrowserView
+from Acquisition import aq_inner
+from Products.CMFCore.utils import getToolByName
 import zipfile
 import tempfile
 import subprocess
 from atreal.massloader.interfaces import IMassLoader
 from lxml import etree
+import hashlib
+from cStringIO import StringIO
 
+from transaction import commit
+
+from collective.quickupload.portlet.quickuploadportlet import JAVASCRIPT
 
 class ProcessError(Exception):
     """Raised if a spawned process terminated with exit code != 0.
@@ -30,16 +37,60 @@ def run_process(cmd, stdin):
 
 class DocxImporter(BrowserView):
 
+    @property
+    def massloader(self):
+        return IMassLoader(self.context)
+
+
     def docx_import(self):
         up_file = self.request.form['doc']
+        filename = up_file.filename
+
+        # We need to save the data *before* extracting images
+        # otherwise it is recognized as a ZIP not as office document
+        data = up_file.read()
+
+        # Extract images and create content out of it
         self._extract_images(up_file)
-        self._extract_html(up_file)
+        commit()
+
+        # Extract text to HTML
+        self._extract_html(data, filename)
+
+        # XXX        IStatusMessage
         self.request.response.redirect(self.context.absolute_url())
 
     def _extract_images(self, up_file):
-        status, log = IMassLoader(self.context).process(up_file, False)
+        status, log = self.massloader.process(up_file, False)
 
-    def _extract_html(self, up_file):
+    def _extract_html(self, up_file, filename):
         cmd = 'java -jar /home/tom/bin/tika-app-1.5.jar -h -'
-        stdout, stderr = run_process(cmd, up_file.read())
-        print stderr, stdout
+        html_result, stderr = run_process(cmd, up_file)
+        parser = etree.HTMLParser()
+        tree = etree.parse(StringIO(html_result), parser)
+        html_body = tree.find('.//body')
+        for img in html_body.findall('.//img'):
+            src = img.attrib['src']
+            if src.startswith("embedded:"):
+                img_name = src[9:]
+            img_obj = self.context.restrictedTraverse('word/media/' + img_name)
+            img.attrib['src'] = 'resolveuid/' + img_obj.UID()
+
+        id = self.massloader._safeNormalize(filename)
+
+        ptypes = getToolByName(aq_inner(self.context), 'portal_types')
+        ptypes.constructContent(type_name='Document',
+                                 container=self.context, id=id, title=filename)
+        doc = self.context[id]
+        doc_body = etree.tostring(html_body).replace('body>', 'div>') # XXXXX
+        doc.setText(doc_body)
+        self.context.setDefaultPage(id)
+
+    def getUploadUrl(self):
+        """ return upload url in current folder """
+        ploneview = self.context.restrictedTraverse('@@plone')
+        folder_url = ploneview.getCurrentFolderUrl()
+        return '%s/@@quick_upload' % folder_url
+
+    def javascript(self):
+        return JAVASCRIPT
